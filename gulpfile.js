6782185256ba817b2200b2dd216d633557c2d65b
americanSammy <34145581+americanSammy@users.noreply.github.com>
2019-01-18 13:02:37 -0500

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -2,26 +2,39 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var browserSync = require('browser-sync').create();
 
+//basic syntax for a gulp task
+gulp.task('task-name', function(done){
+console.log('Hello Zell');
+done();
+});
+
+
 //basic syntax for watching (real time refreshing of files)
 //gulp.watch('files-to-watch', gulp.series('tasks', 'to', 'run')); 
 
 gulp.task('sass', function(done){
   return gulp.src('app/scss/**/*.scss')
     .pipe(sass()) // Converts Sass to CSS with gulp-sass
-    .pipe(gulp.dest('app/css')) //sends files to css folder
-    .pipe(browserSync.stream()) // must come after gulp.dest
+    .pipe(gulp.dest('app/css'))
+    .pipe(browserSync.reload({
+    	stream: true
+    }))
     done();
 });
 
-// Creating a static server?
+
+
 gulp.task('browserSync', function(done){
 	browserSync.init({
 		server:{
 			baseDir: 'app',
+			reloadDelay: 200
 		},
 	})
 	done();
 });
+
+
 
 //task allows addition of more watch functions for different files
 gulp.task('watchSass', function(done){
@@ -30,20 +43,24 @@ gulp.task('watchSass', function(done){
 });
 
 gulp.task('watchHTML', function(done){
-  	gulp.watch('app/**/*.html', browserSync.reload());	
+  	gulp.watch('app/*.html').on('change', browserSync.reload);
     done();
 });
 
 gulp.task('watchJS', function(done){
-	gulp.watch('app/js/**/*.js', browserSync.reload()); 
+	gulp.watch('app/js/**/*.js').on('change', browserSync.reload);
 	done();
 });
 
-// Runs all tasks in parallel in order to update browser in "real time."
+
+
 gulp.task('def', 
 	gulp.series(gulp.parallel('sass','browserSync'), 
-	gulp.parallel('watchSass','watchHTML','watchJS')), function(done){
-done();
-}
+				gulp.parallel('watchSass','watchHTML','watchJS')
+				),
+				function(done){
+					console.log('def has run');
+	    			done();
+							  }
 
 );
